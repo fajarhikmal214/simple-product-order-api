6cbf20fb9686a83f5a22477746d39943ce453fe7
Fajar Hikmal <fajarhikmal214@gmail.com>
2025-05-08 15:00:27 +0700

    chore(migrations): add product table

--- SimpleProductOrderApi/Data/AppDbContext.cs
+++ SimpleProductOrderApi/Data/AppDbContext.cs
@@ -1,9 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using SimpleProductOrderApi.Models;
 
 namespace SimpleProductOrderApi.Data
 {
     public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
- 
+        public DbSet<Product> Products => Set<Product>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasQueryFilter(p => p.DeletedAt == null);
+        }
     }
 }
---
+++ SimpleProductOrderApi/Models/Product.cs
@@ -0,0 +1,14 @@
+namespace SimpleProductOrderApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public int Price { get; set; }
+        
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? DeletedAt { get; set; }
+    }
+}
